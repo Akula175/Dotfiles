508d9d70b4e10f4c425c9a9ac7cc01d4bf621387
Akula175 <tobias.gus99@gmail.com>
2026-03-29 12:59:42 +0200

    Update Script

--- dependencies.sh
+++ dependencies.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-#Installs dependencies needed for Dotfiles, assumes that Noctalia-shell is already installed
+#Checks sudo priveleges
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script with sudo"
+  exit 1
+fi
 
-pacman -S --noconfirm neovim alacritty firefox thunar
+#Adds Home_DIR Var
+USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+
+#Installs dependencies needed for Dotfiles, assumes that Noctalia-shell is already installed
+echo "Installing packages.."
+pacman -S --noconfirm neovim alacritty firefox thunar git
 
 #Installs LazyVim
+echo "Installing LazyVim.."
+git clone https://github.com/LazyVim/starter $USER_HOME/.config/nvim
+rm -rf $USER_HOME/.config/nvim/.git
 
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-rm -rf ~/.config/nvim/.git
-
-echo "Install Done, exiting..."
+echo "Install done, exiting.."
 sleep 2
