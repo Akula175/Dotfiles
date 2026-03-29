218d6f64195ec7c8bc42b3581337f293a12d6c77
Akula175 <tobias.gus99@gmail.com>
2026-03-29 19:38:16 +0200

    Update script and added alacritty

--- dependencies.sh
+++ dependencies.sh
@@ -11,12 +11,24 @@ USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 
 #Installs dependencies needed for Dotfiles, assumes that Noctalia-shell is already installed
 echo "Installing packages.."
-pacman -S --noconfirm neovim alacritty firefox thunar git
+sleep 2
+pacman -S --noconfirm neovim alacritty firefox thunar git fish
 
 #Installs LazyVim
 echo "Installing LazyVim.."
+sleep 2
 git clone https://github.com/LazyVim/starter $USER_HOME/.config/nvim
 rm -rf $USER_HOME/.config/nvim/.git
 
-echo "Install done, exiting.."
+#Moves files into .config Dir
+echo "Install done, copying files.."
+sleep 2
+
+mv ./alacritty $USER_HOME/.config/
+mv ./fastfetch $USER_HOME/.config/
+mv ./fish $USER_HOME/.config/
+mv ./niri $USER_HOME/.config/
+mv ./wallpapers $USER_HOME/Pictures/
+
+echo "Done moving files, exiting install.."
 sleep 2
